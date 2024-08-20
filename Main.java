fb7aaf0f23d0714fb5274f07c565ab444e626044
Angeline Ramos <ange.sofi.rr@gmail.com>
2024-08-20 20:31:34 -0300

    usuario insere alunos

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     
@@ -6,6 +9,9 @@ public class Main {
 
        Academia academia1 = new Academia("Sport Center", "40028922", "Rua Moranguinho, 821");
        Academia academia2 = new Academia("Aquafit", "99765412", "Rua Laranjinha, 501");
+
+       Scanner scr = new Scanner(System.in);
+       List<Aluno> alunos = new ArrayList<>();
 
        System.out.println("Dados iniciais:");
        System.out.println(academia1.getNome() + "\n" + academia1.getTelefone() +  "\n" + academia1.getEndereco() + "\n");
@@ -20,21 +26,60 @@ public class Main {
        academia2.setEndereco("Rua Café Olé, 444");
 
 
+       /*
        //aluno academia 1
        Aluno aluno1 = new Aluno("Gabriela", "05/07/2000", "iniciante");
        academia1.alunos.add(aluno1);
-        //OU
        academia1.alunos.add(new Aluno("Joana", "10/07/2001", "intermediario", "Feminino", 0, 0));
-
        academia1.alunos.add(new Aluno("Pedro",  "19/06/2006", "intermediario"));
-
-
        //alunos academia 2
        academia2.alunos.add(new Aluno("Rafael",  "20/08/1995", "iniciante"));
-
        academia2.alunos.add(new Aluno("Fernanda", "21/12/1999", "iniciante"));
-
        academia2.alunos.add(new Aluno("Marcelo", "27/04/1978", "avançado"));
+       */
+
+       //usuario insere os dados
+
+       
+        String continuar;
+        do {
+            System.out.println("Digite os dados do aluno:");
+
+            System.out.print("Nome: ");
+            String nome = scr.nextLine();
+
+            System.out.print("Data de Nascimento (dd/MM/yyyy): ");
+            String nascimento = scr.nextLine();
+
+            System.out.print("Nível: ");
+            String nivel = scr.nextLine();
+
+            System.out.print("Gênero: ");
+            String genero = scr.nextLine();
+
+            System.out.print("Altura (em metros): ");
+            double altura = Double.parseDouble(scr.nextLine());
+
+            System.out.print("Peso (em kg): ");
+            double peso = Double.parseDouble(scr.nextLine());
+
+            try {
+                Aluno aluno = new Aluno(nome, nascimento, nivel, genero, altura, peso);
+                alunos.add(aluno);
+                System.out.println("Aluno cadastrado com sucesso!");
+
+            } catch (ParseException e) {
+                System.out.println("Erro ao cadastrar aluno: " + e.getMessage());
+            }
+
+            System.out.print("Deseja adicionar outro aluno? (sim/não): ");
+            continuar = scr.nextLine();
+
+        } while (continuar.equalsIgnoreCase("sim"));
+
+        
+
+        scr.close();
 
        
        System.out.println("Dados atualizados:\n");
